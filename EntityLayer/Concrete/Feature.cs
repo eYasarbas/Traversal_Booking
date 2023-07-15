88a8e150628dc766f694b7475320ebe53bb6c44d
eYasarbas <yasarbas583@gmail.com>
2023-07-16 01:00:12 +0300

    added Entity Layer Concrate classes

---
+++ EntityLayer/Concrete/Feature.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EntityLayer.Concrete
+{
+    public class Feature
+    {
+        [Key]
+        public int FeatureID { get; set; }
+        public string Post1Name { get; set; }
+        public string Post1Description { get; set; }
+        public string Post1Image { get; set; }
+        public string Post1Status { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Testimonal.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EntityLayer.Concrete
+{
+    public class Testimonal
+    {
+        [Key]
+        public int TestimonalID { get; set; }
+        public string Client { get; set; }
+        public string Comment { get; set; }
+        public string ClientImage { get; set; }
+        public bool Status { get; set; }
+    }
+}
