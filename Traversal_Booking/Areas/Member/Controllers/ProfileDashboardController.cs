1e0d12ef149062b1d2a9d6647f9d85f0979f419c
Enes Yaşarbaş <yasarbas583@gmail.com>
2023-09-14 12:57:09 +0300

    add profile dashboard

---
+++ Traversal_Booking/Areas/Member/Controllers/ProfileDashboardController.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Traversal_Booking.Areas.Member.Controllers;
+
+[Area("Member")]
+public class ProfileDashboardController : Controller
+{
+    private readonly UserManager<AppUser> _userManager;
+
+    public ProfileDashboardController(UserManager<AppUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var values = await _userManager.FindByNameAsync(User.Identity.Name);
+        ViewBag.userName = values?.Name + " " + values?.SurName;
+        ViewBag.userImage = values.ImageUrl;
+        return View();
+    }
+}
