331ca3aa5e60f0b8f838e31fcfe0649177d91fc3
Enes Yaşarbaş <yasarbas583@gmail.com>
2023-09-14 14:27:54 +0300

    add profile ınformation and partical class

---
+++ Traversal_Booking/ViewComponents/UserDashboard/_ProfileInformation.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Traversal_Booking.ViewComponents.UserDashboard;
+
+public class _ProfileInformation : ViewComponent
+{
+    private readonly UserManager<AppUser> _userManager;
+
+    public _ProfileInformation(UserManager<AppUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var values = await _userManager.FindByNameAsync(User.Identity.Name);
+        ViewBag.memberName = values.Name + " " + values.SurName;
+        ViewBag.memberPhone = values.PhoneNumber;
+        ViewBag.memberMail = values.Email;
+        return View();
+    }
+}
