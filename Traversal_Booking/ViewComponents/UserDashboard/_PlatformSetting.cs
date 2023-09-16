f198dec1793c0eeb3481b471fd550b8baa8fa0b8
Enes Yaşarbaş <yasarbas583@gmail.com>
2023-09-16 16:27:33 +0300

    update profile setting

---
+++ Traversal_Booking/ViewComponents/UserDashboard/_PlatformSetting.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Traversal_Booking.ViewComponents.UserDashboard;
+
+public class _PlatformSetting : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
