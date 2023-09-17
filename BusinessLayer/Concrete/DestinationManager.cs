10317771e20b8555611a93279bd60029b1d5e738
Enes Yaşarbaş <yasarbas583@gmail.com>
2023-09-17 13:43:53 +0300

    Admin add new Routes

--- BusinessLayer/Concrete/DestinationManager.cs
+++ BusinessLayer/Concrete/DestinationManager.cs
@@ -3,53 +3,50 @@ using DataAccessLayer.Abstract;
 using EntityLayer.Concrete;
 using System.Linq.Expressions;
 
-namespace BusinessLayer.Concrete
+namespace BusinessLayer.Concrete;
+
+public class DestinationManager : IDestinationService
 {
-    public class DestinationManager : IDestinationService
+    IDestinationDal _destinationDal;
+
+    public DestinationManager(IDestinationDal destinationDal)
     {
-        IDestinationDal _destinationDal;
-        public DestinationManager(IDestinationDal destinationDal)
-        {
-            _destinationDal = destinationDal;
-
-        }
-
-
-
-        public void TAdd(Destination item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void TDelete(Destination item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<Destination> TGetList()
-        {
-            return _destinationDal.GetList();
-        }
-
-        public void TUpdate(Destination item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Destination GetById(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Destination TGetById(int id)
-        {
-            return _destinationDal.GetByID(id);
-        }
-
-        public List<Destination> GetByFilter(Expression<Func<Destination, bool>> filter)
-        {
-            throw new NotImplementedException();
-        }
+        _destinationDal = destinationDal;
     }
 
+
+    public void TAdd(Destination item)
+    {
+        _destinationDal.Insert(item);
+    }
+
+    public void TDelete(Destination item)
+    {
+        _destinationDal.Delete(item);
+    }
+
+    public List<Destination> TGetList()
+    {
+        return _destinationDal.GetList();
+    }
+
+    public void TUpdate(Destination item)
+    {
+        _destinationDal.Update(item);
+    }
+
+    public Destination GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Destination TGetById(int id)
+    {
+        return _destinationDal.GetByID(id);
+    }
+
+    public List<Destination> GetByFilter(Expression<Func<Destination, bool>> filter)
+    {
+        throw new NotImplementedException();
+    }
 }
--- Traversal_Booking/Areas/Admin/Controllers/DestinationController.cs
+++ Traversal_Booking/Areas/Admin/Controllers/DestinationController.cs
@@ -1,12 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Traversal_Booking.Areas.Admin.Controllers;
 
 [Area("Admin")]
 public class DestinationController : Controller
 {
+    DestinationManager _destinationManager = new(new EfDestinationDal());
+
+
+
     public IActionResult Index()
     {
+        var values = _destinationManager.TGetList();
+        return View(values);
+    }
+
+    [HttpGet]
+    public IActionResult AddDestination()
+    {
         return View();
+    }
+
+    [HttpPost]
+    public IActionResult AddDestination(Destination destination)
+    {
+        _destinationManager.TAdd(destination);
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpPost]
+    public IActionResult DeleteDestination(int id)
+    {
+        var values = _destinationManager.GetById(id);
+        _destinationManager.TDelete(values);
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public IActionResult UpdateDestination(int id)
+    {
+        var values = _destinationManager.GetById(id);
+
+        return View(values);
+    }
+
+    [HttpPost]
+    public IActionResult UpdateDestination(Destination destination)
+    {
+        _destinationManager.TUpdate(destination);
+        return RedirectToAction(nameof(Index));
     }
 }
