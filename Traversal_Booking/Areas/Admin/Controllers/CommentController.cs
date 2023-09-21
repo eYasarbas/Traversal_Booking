c1fb775516f1f6486436ae8ce3b1079194eea18e
Enes Yaşarbaş <yasarbas583@gmail.com>
2023-09-21 17:20:53 +0300

    reducing efcore dependencies also in the admin comment dashboard

--- Traversal_Booking/Areas/Admin/Controllers/CommentController.cs
+++ Traversal_Booking/Areas/Admin/Controllers/CommentController.cs
@@ -1,5 +1,4 @@
-﻿using BusinessLayer.Concrete;
-using DataAccessLayer.EntityFramework;
+﻿using BusinessLayer.Abstract;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Traversal_Booking.Areas.Admin.Controllers;
@@ -7,18 +6,22 @@ namespace Traversal_Booking.Areas.Admin.Controllers;
 [Area("Admin")]
 public class CommentController : Controller
 {
-    private readonly CommentManager _commentManager = new(new EfCommentDal());
+    private readonly ICommentService _commentService;
 
+    public CommentController(ICommentService commentService)
+    {
+        _commentService = commentService;
+    }
     public IActionResult Index()
     {
-        var values = _commentManager.TGetListCommentsWithDestination();
+        var values = _commentService.TGetListCommentsWithDestination();
         return View(values);
     }
 
     public IActionResult DeleteComment(int id)
     {
-        var values = _commentManager.TGetById(id);
-        _commentManager.TDelete(values);
+        var values = _commentService.TGetById(id);
+        _commentService.TDelete(values);
         return RedirectToAction(nameof(Index));
     }
 }
