0cc0174193bc661b2690478d921abab595840204
Enes Yaşarbaş <yasarbas583@gmail.com>
2023-12-13 23:32:45 +0300

    Send Mail Configuration

---
+++ Traversal_Booking/Areas/Admin/Controllers/MailController.cs
@@ -0,0 +1,37 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using Traversal_Booking.Models;
+
+namespace Traversal_Booking.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class MailController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Index(MailRequest request)
+    {
+        MimeMessage message = new();
+        var mailboxAddressFrom = new MailboxAddress(request.Name, "");
+        message.From.Add(mailboxAddressFrom);
+        var mailboxAddressTo = new MailboxAddress("User", request.RecieverMail);
+        message.To.Add(mailboxAddressTo);
+        message.Subject = request.Subject;
+        var bodyBuilder = new BodyBuilder();
+        bodyBuilder.TextBody = request.Body;
+        message.Body = bodyBuilder.ToMessageBody();
+        //message.Body = 
+        var client = new SmtpClient();
+        client.Connect("smtp.gmail.com", 587, false);
+        client.Authenticate("", "");
+        client.Send(message);
+        client.Disconnect(true);
+        return View();
+    }
+}
---
+++ Traversal_Booking/Models/MailRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Traversal_Booking.Models
+{
+    public class MailRequest
+    {
+        public string Name { get; set; }
+        public string SenderMail { get; set; }
+        public string RecieverMail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
